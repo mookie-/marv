ccc4dcc97e585f4e07c2c8893e692772cca7c1e2
Kim Jahn <kim.jahn@makandra.de>
2013-01-18 11:25:30 +0100

    add licence

--- marv.rb
+++ marv.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/ruby
+#
+#   * ----------------------------------------------------------------------------
+#   * "THE BEER-WARE LICENSE" (Revision 42):
+#   * <kim@maisspace.org> wrote this file. As long as you retain this notice you
+#   * can do whatever you want with this stuff. If we meet some day, and you think
+#   * this stuff is worth it, you can buy me a beer in return Kim
+#   * ----------------------------------------------------------------------------
+#
 
 require 'thread'
 require 'yaml'
